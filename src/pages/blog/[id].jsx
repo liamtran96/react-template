1f86366420579b00e2a87aaf2f20002d4bef9706
Liam Tran <111833625+liamtran96@users.noreply.github.com>
2023-09-16 06:37:24 +0000

    feat: update source code

--- src/pages/blog/[id].jsx
+++ src/pages/blog/[id].jsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom";
-// eslint-disable-next-line react/prop-types
+
 export default function PageName() {
   const { id } = useParams();
   return <div>Blog {id}</div>;
